00d1bb17654b19efa0f743e41a80772fa2a5e835
Gabriel Araujo <gabriel_scf@hotmail.com>
2023-03-21 15:54:26 -0300

    Update our backend-generated 'Types' file.

--- src/Types.ts
+++ src/Types.ts
@@ -13,6 +13,7 @@ interface ISpeciesInformation {
   speciesInformationGenus: string | null;
   speciesInformationFamily: string | null;
   speciesInformationStatuses: string[];
+  speciesInformationScientificName: string;
   speciesInformationVernacularNames: VernacularName[];
 }
 
@@ -20,10 +21,65 @@ type RemoteResult = IRemoteResult;
 
 interface IRemoteResult {
   remoteResultOriginalQuery: string;
-  remoteResultInformation: SpeciesInformation;
   remoteResultScientificName: string;
-  remoteResultImages: string[];
-  remoteResultWikipedia: string | null;
+  remoteResultInformation: SpeciesInformation;
+  remoteResultImages: RemoteContent<string[]>;
+  remoteResultWikipedia: RemoteContent<string>;
 }
 
-export type { RemoteResult, SpeciesInformation };
+type GameAnswer = IGameAnswer;
+
+interface IGameAnswer {
+  speciesGroups: string[][];
+  answerTaxonomicDiscriminators: TaxonomicDiscriminators;
+}
+
+type GameSetup = IGameSetup;
+
+interface IGameSetup {
+  species: RemoteResult[];
+  nbGroups: number;
+  textTip: string;
+  gameTaxonomicDiscriminators: TaxonomicDiscriminators;
+}
+
+type GameResult = IGameResult;
+
+interface IGameResult {
+  gameResultSuccess: boolean;
+  gameResultScore: number;
+  gameResultcorrectAnswer: string[][];
+}
+
+type NewGameRequest = INewGameRequest;
+
+interface INewGameRequest {
+  speciesNumber: number;
+  groupNumber: number;
+  speciesGroup: number;
+  gameDifficulty: number;
+}
+
+type RemoteContent<T> = IRetrieved<T> | INotAvailable<T> | INeverTried<T>;
+
+interface IRetrieved<T> {
+  tag: "Retrieved";
+  contents: T;
+}
+
+interface INotAvailable<T> {
+  tag: "NotAvailable";
+}
+
+interface INeverTried<T> {
+  tag: "NeverTried";
+}
+
+type TaxonomicDiscriminators = ITaxonomicDiscriminators;
+
+interface ITaxonomicDiscriminators {
+  rootDiscriminator: number;
+  groupDiscriminator: number;
+}
+
+export type { RemoteResult, GameAnswer, GameSetup };
